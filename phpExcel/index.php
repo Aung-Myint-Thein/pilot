1fb7923afd057c88b69e80e407cd3265df83778d
Aung Myint Thein Aug 2012 Key <thein.aungmyint@gmail.com>
2013-01-10 17:30:33 +0800

    download mysql into excel. Used tutorial

--- phpExcel/index.php
+++ phpExcel/index.php
@@ -1,24 +1,30 @@
+<html>
+	<head>
+		<title>Pilot Program</title>
+	</head>
+<body>
+
 <h1>Test Driving MySqlExcelBuilder</h1>
 <?php 
 require_once('MySqlExcelBuilder.class.php');
 
 // Intialize the object with the database variables
-$database = 'xls_sample';
+$database = 'test';
 $user='root';
 $pwd='insead2012';
 $mysql_xls = new MySqlExcelBuilder($database,$user,$pwd);
 
 // Setup the SQL Statements
-$sql_statement = 'select * from customer;';
+$sql_statement = 'select * from atmmachines;';
 
 
-$sql_statement2 = 'select * from order_item;';
+$sql_statement2 = 'select * from country;';
 
 
 
 // Add the SQL statements to the spread sheet
-$mysql_xls->add_page('Gold Mugs',$sql_statement,'','B',2);
-$mysql_xls->add_page('Tea',$sql_statement2,'','B',2);
+$mysql_xls->add_page('ATM Machines',$sql_statement,'','B',2);
+$mysql_xls->add_page('Country',$sql_statement2,'','B',2);
 
 // Get the spreadsheet after the SQL statements are built...
 $phpExcel = $mysql_xls->getExcel(); // This needs to come after all the pages have been added.
@@ -50,3 +56,6 @@ echo "<a href=\"$fname\">Download $fname</a>";
 
 
 ?>
+
+</body>
+</html>
