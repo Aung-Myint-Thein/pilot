6f7029ad7546c28423e100456971167ba196066e
Aung Myint Thein <myintthein.aung@insead.edu>
2013-01-24 17:22:42 +0800

    removed all formatting

--- test.php
+++ test.php
@@ -35,24 +35,7 @@ $mysql_xls->add_page('Internet Users',$sql_statement2,'','B',2);
 
 // Get the spreadsheet after the SQL statements are built...
 $phpExcel = $mysql_xls->getExcel(); // This needs to come after all the pages have been added.
-/*
-$phpExcel->setActiveSheetIndex(0); // Set the sheet to the first page.
-// Do some addtional formatting using PHPExcel
-$sheet = $phpExcel->getActiveSheet();
-$date = date('Y-m-d');
-$cellKey = "A1"; 
-$sheet->setCellValue($cellKey,"Data downloaded on $date");
-$style = $sheet->getStyle($cellKey);                              
-$style->getFont()->setBold(true);
 
-$phpExcel->setActiveSheetIndex(1); // Set the sheet to the second page.
-$sheet = $phpExcel->getActiveSheet(); 
-$sheet->setCellValue($cellKey,"Data downloaded on $date");
-$style = $sheet->getStyle($cellKey);                              
-$style->getFont()->setBold(true);
-
-$phpExcel->setActiveSheetIndex(0); // Set the sheet back to the first page.
-*/
 // Write the spreadsheet file...
 $objWriter = PHPExcel_IOFactory::createWriter($phpExcel, 'Excel5'); // 'Excel5' is the oldest format and can be read by old programs.
 $fname = "DataFile_".date("Y-m-d His").".xls";
